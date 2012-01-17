96fbd428d6d1031a7fda8e6b1493dcf6a9a10f9f
Gleb Peregud <gleber.p@gmail.com>
2012-01-17 13:51:37 +0100

    initial ssao implementation more refactor

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ UI.keys = {};
 
 function handleKeyDown(event) {
     UI.keys[event.keyCode] = true;
+    //console.log(event.keyCode);
+    if (event.keyCode == 32) {
+        tick();
+    }
 }
 
 function handleKeyUp(event) {
@@ -15,8 +19,7 @@ function handleKeyUp(event) {
 function initGL(canvas) {
     try {
         gl = canvas.getContext("experimental-webgl");
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
+        G3World.setContext(gl, canvas.width, canvas.height);
     } catch (e) {
     }
     assert(gl, "Could not initialise WebGL, sorry :-(");
@@ -25,14 +28,17 @@ function initGL(canvas) {
 
 function initShaders() {
     // new G3Program(gl, "colored", "colored-shader-vs", "colored-shader-fs");
+    new G3Program(gl, "ssao-depth", "showDepth.vert", "showDepth.frag");
+    new G3Program(gl, "ssao-main", "ssao.vert", "ssao.frag");
+    new G3Program(gl, "blur", "baseTexture.vert", "blur.frag");
     new G3Program(gl, "solid-color", "solid-color-shader-vs", "solid-color-shader-fs");
+    new G3Program(gl, "solid-blue-color", "solid-color-shader-vs", "solid-blue-color-shader-fs");
     // new G3Program(gl, "multitextured", "multitextured-shader-vs", "multitextured-shader-fs");
     // new G3Program(gl, "textured", "textured-shader-vs", "textured-shader-fs");
     // new G3Program(gl, "skysphere", "skysphere-vs", "skysphere-fs");
 }
 
 
-var pMatrix = mat4.create();
 
 // -4 18 14
 // -17 -48
@@ -42,22 +48,28 @@ var field_tex;
 var field2_tex;
 var marking_tex;
 
-function initBuffers() {
+function initScene() {
     var red   = [1,0,0,1];
     var green = [0,1,0,1];
     var blue  = [0,0,1,1];
+    var white  = [1,1,1,1];
 
-    var spm = new G3Mesh(gl);
-    spm.setSphere(10);
+    var spm = new G3SphereMesh(gl);
+    spm.init(1);
     var spmm = new G3MeshModel(gl, G3World.programs["solid-color"]);
+    spmm.material.uniforms["uColor"] = green;
     spmm.setMesh(spm);
-    for (var i = 0; i < 1; i++) {
-        var x = (Math.random() - 0.5) * 2.0;
-        var y = (Math.random() - 0.5) * 2.0;
-        var z = (Math.random() - 0.5) * 2.0;
+    for (var i = 0; i < 5; i++) {
+        var x = (Math.random() - 0.5) * 5.0;
+        var y = (Math.random() - 0.5) * 5.0;
+        var z = (Math.random() - 0.5) * 5.0;
         var spo = new G3Object(gl);
         spo.setModel(spmm);
-        spo.translate(x, y, z);
+        spo.animation = new G3WaveAnimation(gl);
+        spo.animation.setPhase(Math.random() * 3);
+        spo.animation.setScale(0.5);
+        spo.translate([x, y, z]);
+        spo.scale([Math.random() * 1.0 + 1.0]);
         G3World.objects.push(spo);
     }
 
@@ -94,55 +106,21 @@ function initBuffers() {
     // G3World.objects.push(bramka2);
 }
 
-function ensureProjection(program, mv) {
-    mv = mv || G3World.mv.current;
-    var normalMatrix = mat3.create();
-    mat4.toInverseMat3(mv, normalMatrix);
-    mat3.transpose(normalMatrix);
-    G3World.setUniform("uNMatrix", normalMatrix);
+
+function drawScene(program) {
+    //G3World.renderScene(program || G3World.programs['solid-blue-color'])
+    G3World.renderScene(program)
 }
 
-function drawScene(width, height, f) {
-    width = width || gl.viewportWidth;
-    height = height || gl.viewportHeight;
-    gl.viewport(0, 0, width, height);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    mat4.perspective(45, width / height, 0.1, 2000.0, pMatrix);
-
-    G3World.mv.push();
-
-    mat4.rotate(G3World.mv.current, degToRad(-G3World.camera.pitch), [1, 0, 0]);
-    mat4.rotate(G3World.mv.current, degToRad(-G3World.camera.yaw), [0, 1, 0]);
-
-    var lightingDirection = [-0.25, -0.25, -1];
-    var adjustedLD = vec3.create();
-    vec3.normalize(lightingDirection, adjustedLD);
-    vec3.scale(adjustedLD, -1);
-
-    var camRotation = mat4.create();
-    mat4.set(G3World.mv.current, camRotation);
-
-    mat4.translate(G3World.mv.current, [-G3World.camera.x, -G3World.camera.y, -G3World.camera.z]);
-
-    G3World.setUniform("uPMatrix", pMatrix);
-
-    G3World.render(camRotation);
-
-    G3World.mv.pop();
-
-    if (f) {
-        f();
-    }
-}
-
-
+var ssao;
 
 function webGLStart() {
     var canvas = document.getElementById("lesson01-canvas");
     initGL(canvas);
     initShaders();
-    initBuffers();
+    initScene();
+    ssao = new G3SSAO(gl);
+    G3World.initProjection();
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -157,6 +135,7 @@ function webGLStart() {
 function tick() {
     requestAnimFrame(tick);
 
+    ssao.renderDepth(drawScene);
     drawScene();
     handleKeys();
     animate();
@@ -213,7 +192,7 @@ function animate() {
             camera.z -= Math.cos(degToRad(camera.pitch)) * Math.cos(degToRad(camera.yaw)) * camera.speed * elapsed;
         }
 
-        // G3World.objects[2].rotate(0.1 * elapsed, 0.05 * elapsed, 0);
+        G3World.animate(elapsed);
 
         camera.yaw += camera.yawSpeed * elapsed;
         camera.pitch += camera.pitchSpeed * elapsed;
